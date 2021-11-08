3b07809be18a3655420478dfbd3380cfe6f4c46c
Nikos Kallergis <nikos.kallergis@networktocode.com>
2021-11-08 14:10:12 +0200

    Refactoring a bit

--- generate_udr.py
+++ generate_udr.py
@@ -1,97 +1,92 @@
 import csv
 from pprint import pprint
 from models.data import RouteTable, Route
+from funcy import omit
 
 
-def read_csv_data(data_file):
-    """Reading CSV file and retuning dictionary of dicts with azure networks info."""
+def read_csv_data(data_file: str) -> dict:
+    """read_csv_data reads a CSV file into a dictionary
+
+    Args:
+        data_file (str): filename of the CSV file
+
+    Returns:
+        dict: Structured representation (dict of dicts) with Azure networks info
+    """
     try:
         with open(data_file, "r") as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=",")
-            next(csv_reader)  # skipping csv header from data
+            
+            # Populate dictionary using headers as keys
             topology_data = {}
+            headers = next(csv_reader)[1:]
             for row in csv_reader:
-                topology_data[row[0]] = {
-                    "location": row[1],
-                    "cidr": row[2],
-                    "type": row[3],
-                }
+                topology_data[row[0]] = {key: value for key, value in zip(headers, row[1:])}
     except IOError:
         print("cannot open file")
     return topology_data
 
 
-def udr_list(topology_data):
-    """For every net/subnet in topology calculates the UDRs and return a full list of UDRs needed.
-    Returns: a List with RouteTable objects.
+def udr_list(topology_data: dict) -> list:
+    """udr_list calculates and returns the UDRs needed for the given data
+
+    Args:
+        topology_data (dict): Dictionary that contains Azure networks info
+
+    Returns:
+        list: List of RouteTable objects
     """
     udr_data = []
 
     for key, value in topology_data.items():
-        if not (value["location"] == "hub" and value["type"] == "vnet"):
+        if (
+            (value["location"] == "hub" and value["type"] == "subnet") or \
+            (value["location"] == "spoke")
+        ):
             udr_data.append(
-                RouteTable(key + "-udr", routes_list(topology_data, key, value))
-            )
-        elif value["location"] == "spoke":
-            udr_data.append(
-                RouteTable(key + "-udr", routes_list(topology_data, key, value))
-            )
-        elif value["location"] == "hub" and value["type"] == "subnet":
-            udr_data.append(
-                RouteTable(key + "-udr", routes_list(topology_data, key, value))
+                RouteTable(
+                    key + "-udr",
+                    routes_list(omit(topology_data, [key]), value["location"]),
+                )
             )
 
     return udr_data
 
 
-def routes_list(topology_data, network_name, network_info):
-    """Generated a list of routes for Specific UDR. Every route is a Route object."""
-    route_list = []
-    route = Route("0.0.0.0_0", "0.0.0.0/0", "VirtualAppliance")
-    route_list.append(route)
+def routes_list(topology_data: dict, src_location: str) -> list:
+    """routes_list generates a list of routes (Route objects) for specific UDR.
 
-    for key, value in topology_data.items():
-        if key != network_name:
-            if network_info["location"] == "hub":
-                if value["location"] == "hub" and value["type"] == "subnet":
-                    route_list.append(
-                        Route(
-                            value["cidr"].replace("/", "_"),
-                            value["cidr"],
-                            "VirtualAppliance",
-                        )
-                    )
-                elif value["location"] == "spoke" and value["type"] == "vnet":
-                    route_list.append(
-                        Route(
-                            value["cidr"].replace("/", "_"),
-                            value["cidr"],
-                            "VirtualAppliance",
-                        )
-                    )
-            else:
-                if value["location"] == "hub" and value["type"] == "vnet":
-                    route_list.append(
-                        Route(
-                            value["cidr"].replace("/", "_"),
-                            value["cidr"],
-                            "VirtualAppliance",
-                        )
-                    )
-                elif value["location"] == "spoke" and value["type"] == "vnet":
-                    route_list.append(
-                        Route(
-                            value["cidr"].replace("/", "_"),
-                            value["cidr"],
-                            "VirtualAppliance",
-                        )
-                    )
+    Args:
+        topology_data (dict): Azure networks info
+        network_name (str): Name of the network that the UDR will attach to
+        network_info (dict): Information about the network at hand
+
+    Returns:
+        list: [description]
+    """
+
+    route_list = [Route("0.0.0.0_0", "0.0.0.0/0", "VirtualAppliance")]
+
+    for dst in topology_data.values():
+        if (
+            (src_location == "hub" and dst["location"] == "hub" and dst["type"] == "subnet") or\
+            (src_location == "hub" and dst["location"] == "spoke" and dst["type"] == "vnet") or\
+            (src_location == "spoke" and dst["location"] == "hub" and dst["type"] == "vnet") or\
+            (src_location == "spoke" and dst["location"] == "spoke" and dst["type"] == "vnet")
+        ):
+            route_list.append(
+                Route(
+                    dst["cidr"].replace("/", "_"),
+                    dst["cidr"],
+                    "VirtualAppliance",
+                )
+            )
 
     return route_list
 
 
 def main():
-    file_location = "/home/memos/Projects/azure-udr-generator/data.csv"
+    file_location = "./data.csv"
     topology_data = read_csv_data(file_location)
     udrs = udr_list(topology_data)
 
--- models/data.py
+++ models/data.py
@@ -1,13 +1,14 @@
 from dataclasses import dataclass
-
-@dataclass
-class RouteTable:
-    name: str
-    routes: list
+from typing import List
 
 @dataclass
 class Route:
     name: str
     dest_subnet: str
     next_hop: str
+
+@dataclass
+class RouteTable:
+    name: str
+    routes: List[Route]
 
